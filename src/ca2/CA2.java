13b623472ecd735cbcd7844cfc092fb5cf3ae0f5
ckell87 <52047301+ckell87@users.noreply.github.com>
2024-02-22 18:39:19 +0000

    created employee class
    Created new employee class. Added required instances. Created two constructers, one default and one with certain parameters.

--- src/ca2/CA2.java
+++ src/ca2/CA2.java
@@ -14,7 +14,9 @@ public class CA2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       class employee {
+        
+        };
     }
     
 }
---
+++ src/ca2/employee/employee.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca2.employee;
+
+/**
+ *
+ * @author under
+ */
+public class employee {     //new employee class
+    public String name;
+    public String email;
+    public int empNum;
+    
+    public employee(){
+        this.name = "default name";
+        this.email = "deafault@default.com";
+        this.empNum = empNum++;
+        
+        
+    }
+    
+}
