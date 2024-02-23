1bea582db662793c9593de6f9821119ca1e51542
ckell87 <52047301+ckell87@users.noreply.github.com>
2024-02-23 17:48:58 +0000

    employee objects
    employee test objects created with required parameters

--- src/ca2/CA2.java
+++ src/ca2/CA2.java
@@ -4,19 +4,32 @@
  */
 package ca2;
 
+import ca2.employee.employee;
+
+ class EmployeeTest { 
+    public static void main(String[] args) {
+        System.out.println("employee test"); 
+         employee emp1 = new employee("Joe Bloggs", "jb@gmail.com");
+         employee emp2 = new employee("Ann Banana", "ab@gmail.com");
+         employee emp3 = new employee("Tom Thumb", "tt@gmail.com");
+         
+         // String[] projectGroup {"emp1", "emp2", "emp3"}; 
+         
+    }   
+    }   
 /**
  *
  * @author under
  */
 public class CA2 {
 
-    /**
-     * @param args the command line arguments
-     */
+
+   
     public static void main(String[] args) {
-       class employee {
+       
+        System.out.println("main method");   
         
-        };
-    }
     
-}
+    }
+}  
+
