a6cfa3a55536d2000e80ddc3a49f6c3b055ef48e
ckell87 <52047301+ckell87@users.noreply.github.com>
2024-02-26 20:22:48 +0000

    ArrayList
    created an ArrayList with 2 constructors, 1 default and one with passed parameters.

---
+++ src/ca2/company/Arraylist.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca2.company;
+
+import ca2.employee.employee;
+
+/**
+ *
+ * @author under
+ */
+class Arraylist<T> {
+
+    void add(employee employee) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
