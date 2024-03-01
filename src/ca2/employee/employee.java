f6965aa7dcede0e8d7e97103d82be9d1112e5d2d
ckell87 <52047301+ckell87@users.noreply.github.com>
2024-03-01 18:59:21 +0000

    cosmetic changes
    changes to indentation and layout for better readability

--- src/ca2/employee/employee.java
+++ src/ca2/employee/employee.java
@@ -9,41 +9,46 @@ package ca2.employee;
  * @author under
  */
 public class employee {     //new employee class
+
     public String name;
     public String email;
     public int empNum;
     public static int nextEmpNum = 1; //Starts emplyee number at 1
-    
-    public employee(){
+
+    public employee() {
         this.name = "default name";
         this.email = "deafault@default.com";
         this.empNum = nextEmpNum++;
     }
-    public employee(String name, String email){
-        this.name =name;
+
+    public employee(String name, String email) {
+        this.name = name;
         this.email = email;
         this.empNum = nextEmpNum++;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
-    }   
-    public String getEmail(){
+    }
+
+    public String getEmail() {
         return email;
     }
-    public int getEmpNum(){
+
+    public int getEmpNum() {
         return empNum;
     }
-    public void setEmail(String email){ //set new email as string once validated
-        if (email.length()>3){
+
+    public void setEmail(String email) { //set new email as string once validated
+        if (email.length() > 3) {
             this.email = email;
         } else {
             System.out.println("Email must be longer than 3 characters");
         }
     }
-    public int getNextEmpNum(){  //return current empNum count/value
+
+    public int getNextEmpNum() {  //return current empNum count/value
         return nextEmpNum;
-        
-        
-        
     }
 }
+
